45414078b7c3a0d3acab558ee6108674f66cd6bf
John Flinchbaugh <john@hjsoft.com>
2023-01-05 10:21:55 -0500

    reduce memory

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 clj \
-    -J-server -J-XX:MaxRAMPercentage=20 -J-XX:MinRAMPercentage=20 \
+    -J-server -J-XX:MaxRAMPercentage=15 -J-XX:MinRAMPercentage=15 \
     -M -m incidents.core "$@"
