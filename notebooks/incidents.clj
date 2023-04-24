44b434717f92387f5b76f98a89f4d85f72a5dbe5
John Flinchbaugh <john@hjsoft.com>
2023-04-23 20:20:40 -0400

    build my own table fn with configurable limit

--- notebooks/incidents.clj
+++ notebooks/incidents.clj
@@ -9,6 +9,23 @@
    [clojure.string :as str]))
 
 ^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
+(defn my-table
+  "display a simple table in html.
+  :head is the sequence of head labels.
+  :rows is a sequence of sequences.
+  :limit is the max to display of the rows."
+  [params]
+  (clerk/html [:table
+               [:thead
+                [:tr
+                 (for [h (:head params)] [:th h])]]
+               [:tbody
+                (for [r (take (or (:limit params) 100) (:rows params))]
+                  [:tr
+                   (for [c r]
+                     [:td c])])]]))
+
+^{:nextjournal.clerk/visibility {:code :hide :result :hide}}
 ^::clerk/no-cache
 (def incidents (with-open [node (start-xtdb!)]
                  (->> node
@@ -17,43 +34,43 @@
                       reverse)))
 
 (clerk/plotly
-  (let [fact-count (->>
-                     incidents
-                     (group-by (fn [i] (format-date (:start-date i))))
-                     (map
-                       (fn [[start-date v]]
-                         [start-date (count v)]))
-                     (sort-by first))]
-    {:data [{:x (map first fact-count)
-             :y (map second fact-count)
-             :type "bar"}]
-     :layout {:title "Incident Count by Date"}}))
+ (let [fact-count (->>
+                   incidents
+                   (group-by (fn [i] (format-date (:start-date i))))
+                   (map
+                    (fn [[start-date v]]
+                      [start-date (count v)]))
+                   (sort-by first))]
+   {:data [{:x (map first fact-count)
+            :y (map second fact-count)
+            :type "bar"}]
+    :layout {:title "Incident Count by Date"}}))
 
 (clerk/plotly
-  (let [fact-count (->>
-                     incidents
-                     (group-by (fn [i] (format-date-part "HH" (:start-date i))))
-                     (map
-                       (fn [[start-date v]]
-                         [start-date (count v)]))
-                     (sort-by first))]
-    {:data [{:x (map first fact-count)
-             :y (map second fact-count)
-             :type "bar"}]
-     :layout {:title "Incident Count by Hour"}}))
+ (let [fact-count (->>
+                   incidents
+                   (group-by (fn [i] (format-date-part "HH" (:start-date i))))
+                   (map
+                    (fn [[start-date v]]
+                      [start-date (count v)]))
+                   (sort-by first))]
+   {:data [{:x (map first fact-count)
+            :y (map second fact-count)
+            :type "bar"}]
+    :layout {:title "Incident Count by Hour"}}))
 
 (clerk/plotly
-  (let [fact-count (->>
-                     incidents
-                     (group-by (fn [i] (format-date-part "e E" (:start-date i))))
-                     (map
-                       (fn [[start-date v]]
-                         [start-date (count v)]))
-                     (sort-by first))]
-    {:data [{:x (map first fact-count)
-             :y (map second fact-count)
-             :type "bar"}]
-     :layout {:title "Incident Count by Day"}}))
+ (let [fact-count (->>
+                   incidents
+                   (group-by (fn [i] (format-date-part "e E" (:start-date i))))
+                   (map
+                    (fn [[start-date v]]
+                      [start-date (count v)]))
+                   (sort-by first))]
+   {:data [{:x (map first fact-count)
+            :y (map second fact-count)
+            :type "bar"}]
+    :layout {:title "Incident Count by Day"}}))
 
 (clerk/plotly
  (let [muni-count (->>
@@ -69,34 +86,36 @@
             :type "bar"}]
     :layout {:title "Incident Count by Municipality"}}))
 
-(clerk/table
-  {:head ["Municipality" "Incidents"]
-   :rows (->>
-           incidents
-           (group-by :municipality)
-           (map
-             (fn [[municipality v]]
-               [municipality (count v)]))
-           (sort-by last)
-           reverse)})
+(my-table
+ {:head ["Municipality" "Count"]
+  :limit 250
+  :rows (->>
+         incidents
+         (group-by :municipality)
+         (map
+          (fn [[municipality v]]
+            [municipality (count v)]))
+         (sort-by last)
+         reverse)})
 
 (clerk/plotly
-  (let [muni-count (->>
-                     incidents
-                     (group-by (fn [i] (cons (:municipality i) (sort (:streets i)))))
-                     (map
-                       (fn [[[municipality & streets] v]]
-                         [(str municipality ": " (str/join " & " streets)) (count v)]))
-                     (sort-by last)
-                     reverse
-                     (take 40))]
-    {:data [{:x (map first muni-count)
-             :y (map second muni-count)
-             :type "bar"}]
-     :layout {:title "Incident Count by Intersection"}}))
+ (let [muni-count (->>
+                   incidents
+                   (group-by (fn [i] (cons (:municipality i) (sort (:streets i)))))
+                   (map
+                    (fn [[[municipality & streets] v]]
+                      [(str municipality ": " (str/join " & " streets)) (count v)]))
+                   (sort-by last)
+                   reverse
+                   (take 40))]
+   {:data [{:x (map first muni-count)
+            :y (map second muni-count)
+            :type "bar"}]
+    :layout {:title "Incident Count by Intersection"}}))
 
-(clerk/table
+(my-table
  {:head ["Municipality" "Intersection" "Incidents"]
+  :limit 250
   :rows (->>
          incidents
          (group-by (fn [i] (cons (:municipality i) (sort (:streets i)))))
@@ -104,9 +123,9 @@
           (fn [[[municipality & streets] v]]
             [municipality
              (clerk/html
-               [:a
-                {:target "_blank" :href (str (map-link municipality streets))}
-                (str/join " & " streets)])
+              [:a
+               {:target "_blank" :href (str (map-link municipality streets))}
+               (str/join " & " streets)])
              (count v)]))
          (sort-by last)
          reverse)})
