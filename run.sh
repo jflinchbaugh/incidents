61eed79c94ad5be7f655d9673563193007ae6ae9
John Flinchbaugh <john@hjsoft.com>
2023-07-16 14:10:17 -0400

    more memory

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 clojure \
-    -J-server -J-XX:MaxRAMPercentage=25 -J-XX:MinRAMPercentage=25 \
+    -J-server -J-XX:MaxRAMPercentage=35 -J-XX:MinRAMPercentage=35 \
     -M -m incidents.core "$@"
